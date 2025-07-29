edf720085cdd1ae7fdb29d1d659db4b79bcb4d57
syed162 <babu.sd15786@gmail.com>
2025-07-29 13:54:50 +0530

    Add files via upload

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 set -e
+
 echo "Running install_dependencies.sh..."
+
 # Check if .NET 8 SDK is already installed
 if dotnet --list-sdks | grep -q "8.0"; then
     echo ".NET 8 SDK already installed."
 else
     echo "Installing .NET 8 SDK..."
-    # Add Microsoft package signing key and repository
-    wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-    sudo dpkg -i packages-microsoft-prod.deb
-    # Update package lists and install .NET SDK 8.0
-    sudo apt-get update
-    sudo apt-get install -y apt-transport-https
-    sudo apt-get update
-    sudo apt-get install -y dotnet-sdk-8.0
+
+    # Install required tools
+    sudo yum install -y wget tar
+
+    # Add Microsoft key and repo
+    sudo rpm -Uvh https://packages.microsoft.com/config/centos/7/packages-microsoft-prod.rpm
+
+    # Install .NET 8 SDK
+    sudo dnf install -y dotnet-sdk-8.0
 
     echo ".NET 8 SDK installed."
 fi
-# Optional: Check installed version
+
 dotnet --version
 echo "install_dependencies.sh completed."
